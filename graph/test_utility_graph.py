2868107cf456914f4664e8f0088ff80bf096a67e
Lane Spangler <las4vc@virginia.edu>
2019-03-16 00:26:58 -0500

    Add max and weighted random methods for selecting the next action

--- graph/test_utility_graph.py
+++ graph/test_utility_graph.py
@@ -1,5 +1,68 @@
 import unittest
-from .utility_graph import UtilityGraph
+from .utility_graph import UtilityGraph, SelectionMethod
+from .action_node import ActionNode
+from .game_stat_node import GameStatNode
+
 
 class TestUtilityGraph(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.graph = UtilityGraph(5)
+        self.graph.top_subset_size = 2
+        self.graph.selection_method = SelectionMethod.MAX
+
+        self.actionA = ActionNode('eat')
+        self.hunger = GameStatNode('hunger')
+        self.actionA.connect_input(self.hunger)
+
+        self.actionB = ActionNode('cry')
+        self.sadness = GameStatNode('sadness')
+        self.actionB.connect_input(self.sadness)
+
+        self.actionC = ActionNode('run')
+        self.energy = GameStatNode('energy')
+        self.actionC.connect_input(self.energy)
+
+        self.actionD = ActionNode('jump')
+        self.actionD.connect_input(self.energy)
+
+        self.graph.actions = {a.id: a for a in [self.actionA,
+                                                self.actionB,
+                                                self.actionC,
+                                                self.actionD]}
+
+    def test_max_selection(self):
+        self.hunger.set_value(0.5)
+        self.sadness.set_value(1.0)
+        self.energy.set_value(0.3)
+
+        action = self.graph.get_next_action()
+        self.assertEqual(action.name, 'cry')
+
+        self.sadness.set_value(0.2)
+        action = self.graph.get_next_action()
+        self.assertEqual(action.name, 'eat')
+
+    def test_get_top_actions(self):
+        self.hunger.set_value(0.5)
+        self.sadness.set_value(1.0)
+        self.energy.set_value(0.3)
+
+        top_actions = self.graph._get_top_actions()
+        self.assertEqual(len(top_actions), 2)
+        for a in top_actions:
+            self.assertTrue(a[1].name in ['eat', 'cry'])
+            self.assertFalse(a[1].name in ['run', 'jump'])
+
+    def test_weight_actions(self):
+        self.hunger.set_value(0.5)
+        self.sadness.set_value(1.0)
+        self.energy.set_value(0.3)
+
+        top_actions = self.graph._get_top_actions()
+        weighted = self.graph._weight_actions_by_utility(top_actions)
+
+        self.assertEqual(len([kv for kv in weighted
+                              if kv[1].name == 'cry']), 100)
+
+        self.assertEqual(len([kv for kv in weighted
+                              if kv[1].name == 'eat']), 50)
--- graph/utility_graph.py
+++ graph/utility_graph.py
@@ -1,7 +1,70 @@
+from typing import List, Tuple
+from uuid import UUID
+from .action_node import ActionNode
+from enum import Enum
+import random
+
+ActionKV = Tuple[UUID, ActionNode]
+
+
 class UtilityGraph:
-    def __init__(self):
-        self.actions = []
-    def get_next_action(self):
+    def __init__(self, seed):
+        self.nodes = {}
+        self.actions = {}
+        self.selection_method = SelectionMethod.MAX
+        self.top_subset_size = 3
+        random.seed(seed)
+
+    def get_next_action(self) -> ActionNode:
         """Get the next action the agent should take based
-        on the current game stats"""
-        pass
+        on the current game stats. Returns the action id"""
+        if not self.actions:
+            return None
+
+        self._reset_all_nodes()
+
+        best_action = {
+            SelectionMethod.MAX: self._max_selection,
+            SelectionMethod.WEIGHTED_RANDOM: self._weighted_random_selection,
+        }
+
+        return best_action[self.selection_method]()
+
+    def _reset_all_nodes(self):
+        """Reset all nodes in the graph, setting
+        the output value to None for all nodes
+        except GameStat and Constant nodes"""
+        for id, action in self.actions.items():
+            action.reset()
+
+    def _max_selection(self):
+        return max(self.actions.items(), key=lambda kv: kv[1].get_output())[1]
+
+    def _weighted_random_selection(self) -> Tuple[str, float]:
+        """Performs a weighted random selection on the 'top_subset_size' actions with
+        the highest utility values."""
+        top_actions = self._weight_actions_by_utility(self._get_top_actions())
+
+        return top_actions[random.randint(0, len(top_actions) - 1)][1]
+
+    def _weight_actions_by_utility(self, actions: List[ActionKV]) \
+            -> List[ActionKV]:
+        """Given a list of actions with normalized utility values (0-1)
+        return a list where an action with utility value n appears
+        100n times in the list"""
+        extended_by_weight = [[kv] * int(kv[1].get_output() * 100)
+                              for kv in actions]
+        return [kv for sublist in extended_by_weight for kv in sublist]
+
+    def _get_top_actions(self) -> List[Tuple[ActionKV]]:
+        """Get the 'top_subset_size' actions with the highest
+        utility values"""
+        sorted_actions = sorted([(k, v) for k, v in self.actions.items()],
+                                reverse=True,
+                                key=lambda kv: kv[1].get_output())
+        return sorted_actions[:self.top_subset_size]
+
+
+class SelectionMethod(Enum):
+    MAX = 1
+    WEIGHTED_RANDOM = 2
