ec0d2d0aa7ac5d5c5d83c2c6430a103a582d4b9c
Lane Spangler <las4vc@virginia.edu>
2019-03-16 00:29:41 -0500

    Fix average node test to account for reset

--- graph/reducer_nodes/test_average_node.py
+++ graph/reducer_nodes/test_average_node.py
@@ -6,17 +6,20 @@ class TestReducerNode(unittest.TestCase):
     def setUp(self):
         self.nodeA = AverageNode()
         self.nodeB = UtilityGraphNode()
-        self.nodeB.output_val = 20
         self.nodeC = UtilityGraphNode()
-        self.nodeC.output_val = 10
 
     def test_get_output(self):
         with self.assertRaises(ValueError):
             self.nodeA.get_output()
 
+        self.nodeB.output_val = 20
+
         self.nodeA.connect_input(self.nodeB)
         self.assertEqual(self.nodeA.get_output(), 20)
         self.nodeA.reset()
+
+        self.nodeB.output_val = 20
+        self.nodeC.output_val = 10
 
         self.nodeA.connect_input(self.nodeC)
         self.assertEqual(self.nodeA.get_output(), 15)
